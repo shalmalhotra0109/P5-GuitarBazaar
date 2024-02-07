926bbcabdb901cc3a137cca3830a435cfdf47e3a
Shal Malhotra <smalhotra0109@gmail.com>
2024-02-07 11:50:51 -0500

    seeded

--- server/seed.py
+++ server/seed.py
@@ -12,13 +12,19 @@ with app.app_context():
             # 'username': ''
             # 'password': ''
             
-            restaurant1 = Restaurant(name='Napex', address='123 Main St, Anytown, USA')
-            restaurant2 = Restaurant(name='PLazusus', address='456 Oak St, Cityville, USA')
+            user1 = Users(id=1, username='shalwuzhere', password='shal123')
+            user2 = Users(id=2, username='peyton319', password='peyton123')
+            user3 = Users(id=3, username='codycoggins', password='cody123')
+            user4 = Users(id=4, username='tommorello', password='tomrox123')
+            user5 = Users(id=5, username='prince80', password='princeiscool123')
+            user6 = Users(id=6, username='chilipeppers1', password='chili')
+            user7 = Users(id=7, username='mickfleetwood', password='silversprings')
+            
 
-            pizza1 = Pizza(name='Veggie Delight', ingredients='Dough, Tomato Sauce, Cheese, Mushrooms, Bell Peppers')
-            pizza2 = Pizza(name='Meat Lovers', ingredients='Dough, Tomato Sauce, Cheese, Pepperoni, Sausage, Bacon')
+            guitar1 = Guitars(name='Veggie Delight', ingredients='Dough, Tomato Sauce, Cheese, Mushrooms, Bell Peppers')
+            pizza2 = Guitars(name='Meat Lovers', ingredients='Dough, Tomato Sauce, Cheese, Pepperoni, Sausage, Bacon')
 
-            db.session.add_all([restaurant1, restaurant2, pizza1, pizza2])
+            db.session.add_all([user1, user2, user3, user4, user5, user6, user7, guitar1])
             db.session.commit()
 
             restaurant_pizza1 = RestaurantPizza(price=12, pizza=pizza1, restaurant=restaurant1)
