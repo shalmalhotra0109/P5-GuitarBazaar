6f8576ebd6de7992767cb8d7d7f8781ab47a2dce
Shal Malhotra <smalhotra0109@gmail.com>
2024-02-13 10:52:10 -0500

    push

---
+++ client/src/components/Exchanges.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+const Exchanges = () => {
+  const [exchanges, setExchanges] = useState([]);
+
+  useEffect(() => {
+    // Make an HTTP request to retrieve the user's exchanges
+    fetch('http://127.0.0.1:5000/exchange/<id>')
+      .then(response => response.json())
+      .then(data => setExchanges(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Exchanges</h1>
+      <ul>
+        {exchanges.map(exchange => (
+          <li key={exchange.id}>{exchange.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Exchanges;
