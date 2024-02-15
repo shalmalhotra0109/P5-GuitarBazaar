90e9e9eafcbeffcc82400bda11125142d11de5a1
Shal Malhotra <smalhotra0109@gmail.com>
2024-02-15 09:29:27 -0500

    styles

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,10 +11,10 @@ const MyNavbar = () => {
         <Container>
           <Navbar.Brand to="/guitars">Guitar Bazaar</Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/guitars">Home</Link>
-            <Link to="/user-likes">Favorites</Link>
-            <Link to={`/guitar/${storedUser.id}`}>User Guitars</Link>
-            <Link to={`/exchange/${storedUser.id}`}>Exchanges</Link>
+            <Nav.Link as={Link} to="/guitars" className="nav-link">Home</Nav.Link>
+            <Nav.Link as={Link} to="/user-likes" className="nav-link">Favorites</Nav.Link>
+            <Nav.Link as={Link} to={`/guitar/${storedUser.id}`} className="nav-link">User Guitars</Nav.Link>
+            <Nav.Link as={Link} to={`/exchange/${storedUser.id}`} className="nav-link">Exchanges</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -22,3 +22,4 @@ const MyNavbar = () => {
 };
 
 export default MyNavbar;
+
